faf28e397d54e50aa17b3d83decd328bdf586f28
Roberto Sangiorge <vimoises39@gmail.com>
2022-09-28 22:14:12 -0300

    Primeiro commit do projeto

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Requisito 1: Escreva a frase "Alô, mundo!"
+        System.out.println("Alô, mundo!");
+    }
+}
